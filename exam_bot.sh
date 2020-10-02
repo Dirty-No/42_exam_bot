b856309e599bcc38fc5aee7a15b88ae6e89d0717
smaccary <smaccary@student.42.fr>
2020-10-02 18:25:03 +0200

    fixed thread

--- exam_bot.sh
+++ exam_bot.sh
@@ -15,15 +15,15 @@ get_grades()
     echo "$(./check_list.sh "$1" "$2")" | sort
 }
 
-START_TIME="0"
-END_TIME="0"
+START_TIME="10"
+END_TIME="18"
 SLEEP_TIME="0"
 
 while [ $(date +"%H") -lt $START_TIME ]; do
     printf "waiting for exam's start... $(date +"(%H:%M:%S)")\r"
 done
 
-LAST_TWEET=$(./tweet.sh "$(printf "$EXAM HAS BEGUN !\nMONITORED PARTICIPANTS :\n$LIST\n" | tee exam.log | tee /dev/tty | head -c 279 | tr '\n' '#' | sed 's/#/%0D/g')")
+#LAST_TWEET=$(./tweet.sh "$(printf "FINALL $EXAM HAS BEGUN !\nMONITORED PARTICIPANTS :\n$LIST\n" | tee exam.log | tee /dev/tty | head -c 279 | tr '\n' '#' | sed 's/#/%0D/g')")
 
 echo "$LAST_TWEET"
 GRADES_OLD=$(get_grades "$1" "$2")
@@ -32,9 +32,12 @@ while [ $(date +"%H") -lt $END_TIME ]; do
 
     GRADES_NEW=$(get_grades "$1" "$2")
 
-    GRADES_DIFF=$(diff <(echo "$GRADES_OLD") <(echo "$GRADES_NEW") | grep '>' | tr '>' '|')
+    GRADES_DIFF=$(diff <(echo "$GRADES_OLD") <(echo "$GRADES_NEW") | grep '>' | tr '>' '|' )
     if [ "$GRADES_DIFF" ];
-        then echo "$GRADES_DIFF" | xargs -L1 ./tweet.sh $(date +"(%H:%M:%S)" | tee /dev/tty | tee --append exam.log)
+       then
+        while IFS= read -r line; do
+    ./tweet.sh "$(printf "%10s%10s" "$(date +"(%H:%M:%S)")" "$line")"
+done <<< "$GRADES_DIFF"
     fi
     GRADES_OLD=$(echo "$GRADES_NEW")
     echo $(date +"(%H:%M:%S)") sleeping...
@@ -46,9 +49,20 @@ SUCCESS=$(echo "$GRADES_NEW" | grep success)
 SUCCESS=$(echo "$SUCCESS" | sort | tee RESULTS.txt)
 FAIL=$(echo "$GRADES_NEW" | grep fail)
 FAIL=$(echo "$FAIL" | sort | tee --append RESULTS.txt)
-FINAL=$(printf "RESULTS:\n\nSUCCESSES:\n%s\nFAILS:\n%s" "$SUCCESS" "$FAIL" | tee --append exam.log)
+FINAL=$(printf "RESULTS:\n\nSUCCESSES:\n%s\nFAILS:\n%s" "$SUCCESS" "$FAIL" | sed '/^$/d' | tee --append exam.log)
+
+DATE="$(date +"(%H:%M:%S)" | tr -d '\n')"
+LAST_TWEET=$(./tweet.sh "$DATE $2 HAS ENDED")
+echo FINAL "$FINAL" END
 
 echo "$FINAL" | while read line ; do
-    echo $line
-    LAST_TWEET=$(./tweet.sh -r 'anonoelle' "$LAST_TWEET" "$line")
+    echo LINE: $line
+    TMP_LINE=$(echo "$line" | tr -d '\n' | tr -d '\r' | tr '\t' ' ')
+    DATE=$(date +"(%H:%M:%S)" | tr -d '\n')
+    MY_LINE=$(echo "$DATE $TMP_LINE")
+    echo MY LINE "$MY_LINE"
+    echo LAST TWEET "$LAST_TWEET" END
+    LAST_TWEET=$(./tweet.sh -r 'anonoelle' "$LAST_TWEET" "$MY_LINE")
+    echo NEW TWEET "$LAST_TWEET" END
+    sleep 2
 done
--- tweet.sh
+++ tweet.sh
@@ -1,37 +1,70 @@
 #!/bin/sh
 
+VAL="3"
+
+printf "ARGS: $1 , $2 , $3 , $4 \n" >> tweet.log
 
 if [ "$1" = "-r" ];
-then
+	then
+			NAME="$2"
+			TWEET_ID="$3"
+			TWEET="$4"
+			TOKEN=$(curl -s -b cookies_twitter.txt -c cookies_twitter.txt "https://mobile.twitter.com/$NAME/reply/$TWEET_ID?p=r" \
+				-H "referer: https://mobile.twitter.com/$NAME/status/$TWEET_ID?p=v" \
+				--compressed | grep authenticity_token | sed '/value="/!d;s//&\n/;s/.*\n//;:a;/"/bb;$!{n;ba};:b;s//\n&/;P;D' | tail -1)
+			sleep 2
+			while [ "$VAL" -gt '0' ] ;do
+			TWEET_PAGE=$(curl -s -L -b cookies_twitter.txt -c cookies_twitter.txt 'https://mobile.twitter.com/compose/tweet' \
+				-H "referer: https://mobile.twitter.com/$NAME/reply/$TWEET_ID?p=r" \
+				--data-raw "authenticity_token=$TOKEN&tweet%5Bin_reply_to_status_id%5D=$TWEET_ID&tweet%5Btext%5D=$TWEET&wfa=1&commit=Reply" \
+				--compressed)
+				if [ -z "$(echo "$TWEET_PAGE" | grep 403)" ];
+					then VAL="0"
+					else
+						printf "403 ON $TWEET \n $TWEET_PAGE \n TOKEN : $TOKEN \n END \n" >> tweet.log
+						sleep 5
+						VAL=$(expr $VAL - 1)
+				fi
+			done
+		else
+			TOKEN=$(curl -s -b cookies_twitter.txt -c cookies_twitter.txt 'https://mobile.twitter.com/compose/tweet' \
+				-H 'referer: https://mobile.twitter.com/' \
+				--compressed | grep authenticity_token | sed '/value="/!d;s//&\n/;s/.*\n//;:a;/"/bb;$!{n;ba};:b;s//\n&/;P;D' | tail -1)
 
-  NAME="$2"
-  TWEET_ID="$3"
-  TWEET="$4"
-  TOKEN=$(curl -s -b cookies_twitter.txt -c cookies_twitter.txt "https://mobile.twitter.com/$NAME/reply/$TWEET_ID?p=r" \
-    -H "referer: https://mobile.twitter.com/$NAME/status/$TWEET_ID?p=v" \
-    --compressed | grep authenticity_token | sed '/value="/!d;s//&\n/;s/.*\n//;:a;/"/bb;$!{n;ba};:b;s//\n&/;P;D' | tail -1)
-  
-  TWEET_PAGE=$(curl -s -L -b cookies_twitter.txt -c cookies_twitter.txt 'https://mobile.twitter.com/compose/tweet' \
-    -H "referer: https://mobile.twitter.com/$NAME/reply/$TWEET_ID?p=r" \
-    --data-raw "authenticity_token=$TOKEN&tweet%5Bin_reply_to_status_id%5D=$TWEET_ID&tweet%5Btext%5D=$TWEET&wfa=1&commit=Reply" \
-    --compressed)
-  
-else
-  TOKEN=$(curl -s -b cookies_twitter.txt -c cookies_twitter.txt 'https://mobile.twitter.com/compose/tweet' \
-    -H 'referer: https://mobile.twitter.com/' \
-    --compressed | grep authenticity_token | sed '/value="/!d;s//&\n/;s/.*\n//;:a;/"/bb;$!{n;ba};:b;s//\n&/;P;D' | tail -1)
+			TWEET="$1"
 
-  TWEET="$1"
-
-  TWEET_PAGE=$(curl -s -L -b cookies_twitter.txt -c cookies_twitter.txt 'https://mobile.twitter.com/compose/tweet' \
-    -H 'referer: https://mobile.twitter.com/compose/tweet' \
-    --data-raw "authenticity_token=$TOKEN&tweet%5Btext%5D=$TWEET+&wfa=1&commit=Tweet" \
-    --compressed)
+			TWEET_PAGE=$(curl -s -L -b cookies_twitter.txt -c cookies_twitter.txt 'https://mobile.twitter.com/compose/tweet' \
+				-H 'referer: https://mobile.twitter.com/compose/tweet' \
+				--data-raw "authenticity_token=$TOKEN&tweet%5Btext%5D=$TWEET+&wfa=1&commit=Tweet" \
+				--compressed)
 fi
 
-TWEET_ID=$(echo "$TWEET_PAGE" | grep -B1 "$TWEET" | grep -E -o [0-9] | tr -d '\n')
+convert_utf8()
+{
+	echo "$1"	\
+	|	sed 's/\;/_MY_SEMICOLON_LABEL_/g'				|	sed 's/#/_MY_HASHTAG_LABEL_/g' | sed 's/\&//g'	      \
+	|	sed 's/%/\&#25\;/g'		|	sed 's/!/\&#21\;/g'	|	sed "s/'/\&#39;/g"                       \
+	|	sed 's/"/\&#22\;/g'		|	sed 's/\$/\&#24\;/g'	\
+	|	sed 's/+/\&#2B\;/g'		|	sed 's/,/\&#2C\;/g'	| 	sed 's/\//\&#2F\;/g'       \
+	|	sed 's/_MY_SEMICOLON_LABEL_/\&#3B\;/g'			|	sed 's/</\&#3C\;/g'		| 	sed 's/=/\&#3D\;/g'		\
+	|	sed 's/>/\&#3E\;/g'		|	sed 's/?/\&#3F\;/g'	|	sed 's/@/\&#40\;/g'		| 	sed 's/\[/\&#5B\;/g'	      \
+	|	sed 's/]/\&#5D\;/g'		|	sed 's/\^/\&#5E\;/g'	|	sed 's/`/\&#60\;/g' | sed 's/ *$//g'	      \
+	| 	sed 's/_MY_HASHTAG_LABEL_/#/g'
+}
+
+
+TWEET=$(convert_utf8 "$TWEET")
+TWEET_ID=$(echo "$TWEET_PAGE" | grep -B1 "$TWEET")
+echo TWEET : "$TWEET" FOUND: "$TWEET_ID" >> tweet.log
+TWEET_ID=$(echo "$TWEET_ID" | sed "s/$TWEET//g" | grep -E -o [0-9] | tr -d '\n')
 if [ -z "$TWEET_ID" ];
-then echo "$TWEET_PAGE"
-else echo "$TWEET_ID"
+then 
+	echo "$TWEET_PAGE" >> tweet.log
+	echo "FAIL : $TWEET_ID END"
+	echo "FAIL $TWEET" >> tweet.log
+	echo "PAGE: $TWEET_PAGE" >> tweet.log
+else 
+	echo "$TWEET_ID"
 fi
 
+sleep 5
