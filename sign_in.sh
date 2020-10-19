d48b3c01643777a5faee76d265ee8924d58b563a
Noëlle <54525684+Dirty-No@users.noreply.github.com>
2020-10-19 14:34:18 +0200

    Update sign_in.sh

--- sign_in.sh
+++ sign_in.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-chmod a+x *.sh
+convert_utf8()
+{
+	echo "$1"	\
+	|	sed 's/%/%25/g'		|	sed 's/\&/%26/g'	|	sed 's/!/%21/g'	\
+	|	sed 's/"/%22/g'		|	sed 's/#/%23/g'		|	sed 's/\$/%24/g'	|	sed 's/(/%28/g'		\
+	|	sed 's/)/%29/g'		|	sed 's/+/%2B/g'		|	sed 's/,/%2C/g'		| 	sed 's/\//%2F/g'	\
+	|	sed 's/:/%3A/g'		|	sed 's/;/%3B/g'		|	sed 's/</%3C/g'		| 	sed 's/=/%3D/g'		\
+	|	sed 's/>/%3E/g'		|	sed 's/?/%3F/g'		|	sed 's/@/%40/g'		| 	sed 's/\[/%5B/g'	\
+	|	sed 's/]/%5D/g'		|	sed 's/\^/%5E/g'	|	sed 's/`/%60/g' 	|	sed 's/{/%7B/g'		\
+	|	sed 's/|/%7C/g'		|	sed 's/}/%7D/g'
+}
 
 printf "Login: "
 read LOGIN
@@ -14,12 +24,15 @@ printf "\n"
 echo "" > cookies.txt
 
 TOKEN=$(curl -s -c cookies.txt -b cookies.txt 'https://signin.intra.42.fr/users/sign_in' --compressed)
-TOKEN=$(echo "$TOKEN" | sed '/<meta name="csrf-token" content="/!d;s//&\n/;s/.*\n//;:a;/"/bb;$!{n;ba};:b;s//\n&/;P;D')
-TOKEN=$(echo "$TOKEN" | sed 's/+/%2B/g' | sed 's/\//%2F/g' | sed 's/=/%3D/g')
+TOKEN=$(echo "$TOKEN" |  grep csrf-token | sed 's/<meta name="csrf-token" content="//g' | sed 's/" \/>//g')
+TOKEN=$(convert_utf8 "$TOKEN")
+
+
+PASSWORD=$(convert_utf8 "$PASSWORD")
 
 curl -s -c cookies.txt -b cookies.txt 'https://signin.intra.42.fr/users/sign_in' \
 --data-raw "utf8=%E2%9C%93&authenticity_token=$TOKEN&user%5Blogin%5D=$LOGIN&user%5Bpassword%5D=$PASSWORD&commit=Sign+in" \
---compressed > /dev/null
+--compressed > response.html
 
 CHECK="$(curl -s -b cookies.txt https://profile.intra.42.fr/)"
 CHECK=$(echo "$CHECK" | grep "Intra Profile Home")
